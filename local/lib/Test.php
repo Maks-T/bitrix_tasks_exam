65655b9325c204f2a1a93527873eff79d8b97b29
Maxim Tsatsura <maxim.tsatsura@gmail.com>
2022-12-31 12:35:13 +0300

    feat: add template tsatsura and namespace

---
+++ local/lib/Test.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace TS;
+
+
+class Test {
+  public function print($data)
+  {
+    echo '<pre>';
+    print_r($data);
+    echo '</pre>';
+  }
+}
---
+++ local/php_interface/init.php
@@ -0,0 +1,11 @@
+<?php
+
+use \Bitrix\Main\Loader;
+
+/**
+ * Include own namespace
+ */
+Loader::registerNamespace(
+ "TS",
+ "local/lib",
+);
