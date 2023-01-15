23c84f76e42978168f857c687d3e1911ce4819b9
Maxim Tsatsura <maxim.tsatsura@gmail.com>
2023-01-15 12:21:31 +0300

    feat: add config meta_template_smart_filter

--- bitrix/.settings_extra.php
+++ bitrix/.settings_extra.php
@@ -1,1 +1,8 @@
-<?php
+<?
+
+return array (
+  'meta_template_smart_filter' => array(
+    'value' => '{h1 | title} + {price} - {price.value} + {property.name} - {property.value}',
+    'readonly' => true,
+  )
+);
--- local/components/tsatsura/meta.smart.filter/class.php
+++ local/components/tsatsura/meta.smart.filter/class.php
@@ -1,20 +1,37 @@
 <?
 
+declare(strict_types=1);
+
+use Bitrix\Main\Config\Configuration;
+
 class MetaSmartFilter extends \CBitrixComponent
 {
 
   public function getFilterValues()
   {
-    $res = $this->arResult['SECTION_NAME'];
+
+    $res = Configuration::getValue('meta_template_smart_filter');
+
+    $res = str_replace('{h1 | title}', $this->arResult['SECTION_NAME'], $res);
 
     if (!empty($this->arResult['PRICE'])) {
-      $res .= ' + { Цена } - { от ' . $this->arResult['PRICE']['FROM']
-        . ' до ' . $this->arResult['PRICE']['TO'] . '} ';
+      $priceValue = 'от ' . $this->arResult['PRICE']['FROM'] .
+        ' до ' . $this->arResult['PRICE']['TO'];
+
+      $res = str_replace(['{price}', '{price.value}'], ['Цена', $priceValue], $res);
+    } else {
+      $res = str_replace('+ {price} - {price.value} ', '', $res);
     }
 
-    foreach ($this->arResult['PROPERTIES'] as $prop) {
-      $res .= ' + {' . $prop['PROPERTY_NAME'] . '} - {' .
-        implode(',', $prop['VALUES']) . '} ';
+    if (!empty($this->arResult['PROPERTIES'])) {
+      $properties = '';
+      foreach ($this->arResult['PROPERTIES'] as $prop) {
+        $properties .= ' + ' . $prop['PROPERTY_NAME'] . ' - ' .
+          implode(',', $prop['VALUES']);
+      }
+      $res = str_replace('+ {property.name} - {property.value}', $properties, $res );
+    } else {
+      $res = str_replace('+ {property.name} - {property.value}', '', $res);
     }
 
     return $res;
